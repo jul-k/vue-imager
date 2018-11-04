6508b3dfb190711c78513ec88eee0aeb27023180
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2018-11-04 18:26:49 +0200

    Wiring up vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App";
 import store from "./store";
+import VueRouter from "vue-router";
+import AuthHandler from "./components/AuthHandler";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        { path: "/oauth2/callback", component: AuthHandler }
+    ]
+});
 
 new Vue({
+    router,
     store, // ES6 syntax (instead of store: store)
     render: h => h(App)
 }).$mount("#app");
