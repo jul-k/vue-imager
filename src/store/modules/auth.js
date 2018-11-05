3b38f1ece5ebe6e5a23d35175aee1d59d077322d
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2018-11-05 21:22:42 +0200

    Parsing the access token

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from "../../api/imgur";
+import qs from "qs";
 
 const state = {
     token: null
@@ -17,6 +18,10 @@ const mutations = {
 const actions = {
     login: () => {
         api.login();
+    },
+    finalizeLogin: ({ commit }, hash) => {
+        const query = qs.parse(hash.replace("#", ""));
+        commit("setToken", query.access_token);
     },
     logout: ({ commit }) => {
         commit("setToken", null);
