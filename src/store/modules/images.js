a81d19ad7559ea72ece07ec3a7e1fa0ad9410be0
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2018-11-17 11:04:32 +0200

    Images module implementation

---
+++ src/store/modules/images.js
@@ -0,0 +1,19 @@
+const state = {
+    images: []
+};
+
+const getters = {
+    allImages: state => state.images
+};
+
+const mutations = {
+    setImages: (state, images) => {
+        state.images = images;
+    }
+};
+
+const actions = {
+    fetchImages() {
+        
+    }
+};
